3d28c5296eb4e706d918faba219c7742ca5ab9e1
lovemilk <zhuhansan666@outlook.com>
2026-07-15 19:47:05 +0800

    fix: general patch not ran in workflow

--- local/patch_general.sh
+++ local/patch_general.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
+echo ">>> 正在进行通用 Patch..."
+
 wget https://github.com/cctv18/oppo_oplus_realme_sm8650/raw/refs/heads/main/cve_patch/CVE-2026-43499.patch
-patch -p1 -F 3 < CVE-2026-43499.patch
+patch -p1 -F 3 < CVE-2026-43499.patch || true
