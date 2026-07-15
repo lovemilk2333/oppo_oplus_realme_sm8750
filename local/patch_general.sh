935b2abf7a6d39d7ee599151c6e8bd7186ee54fd
lovemilk <zhuhansan666@outlook.com>
2026-07-15 20:04:06 +0800

    fix: invalid workpath of general patch (again and again)

--- local/patch_general.sh
+++ local/patch_general.sh
@@ -3,8 +3,5 @@ set -e
 
 echo ">>> 正在进行通用 Patch..."
 
-cd kernel_workspace || exit 255
-cd common || exit 255
-
 wget https://raw.githubusercontent.com/lovemilk2333/oppo_oplus_realme_sm8750/refs/heads/main/cve_patch/CVE-2026-43499.patch
 patch -p1 -F 3 < CVE-2026-43499.patch || true
